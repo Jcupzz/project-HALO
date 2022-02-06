2a801c809f074f7be3fd8f3ad67d9d5108d7c20c
Jcupzz <jcupzz255@gmail.com>
2022-02-06 12:02:04 +0530

    changed to project-halo (the beginning)

---
+++ lib/Halo.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Halo extends StatefulWidget {
+  const Halo({Key? key}) : super(key: key);
+
+  @override
+  _HaloState createState() => _HaloState();
+}
+
+class _HaloState extends State<Halo> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
