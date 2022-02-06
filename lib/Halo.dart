ccfda676fe78c3b6a0cabc759eee5290a044b0bd
Jcupzz <jcupzz255@gmail.com>
2022-02-06 13:26:52 +0530

    added idleRight action

--- lib/Halo.dart
+++ lib/Halo.dart
@@ -1,3 +1,20 @@
+import 'package:flame/components.dart';
 import 'package:flame/game.dart';
+import 'package:flame/sprite.dart';
+import 'package:project_halo/actions/Idle_Right.dart';
 
-class Halo extends FlameGame {}
+class Halo extends FlameGame {
+  @override
+  Future<void>? onLoad() async {
+    // TODO: implement onLoad
+
+    print(size.x);
+    print(size.x / 2);
+
+    final idleRight = IdleRight(Vector2(size.x / 2, size.y / 2));
+
+    add(idleRight);
+
+    return super.onLoad();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,17 @@
+import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 import 'package:project_halo/Halo.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  final game = Halo();
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const Halo(),
-    );
+    return GameWidget(game: game);
   }
 }
